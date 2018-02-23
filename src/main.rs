ac3c04e2a41169983f85774f8ecf58b2bac07803
ThermalSpan <russell@RusselllMacBook.hsd1.ca.comcast.net>
2018-02-22 23:01:06 -0800

    Some cleanup

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,20 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-	let argv : Vec<String> = env::args().collect();
+    let argv : Vec<String> = env::args().collect();
     let mut path_buf = PathBuf::from(&argv[1]);
 
     let mut keep_going = true;
     while keep_going {
         if path_buf.exists() {
             print!("✔");
-            keep_going = false;
+            break;
         } else {
             print!("✘");
         }
 
         println!(" {}", path_buf.display());
 
-        keep_going = path_buf.pop() && keep_going;
+        keep_going = path_buf.pop();
     }
 }
